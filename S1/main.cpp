817351ea8a118ebe26e26cc093667ee6fedcb167
Amos Brocco <amos.brocco@gmail.com>
2025-09-23 08:07:44 +0200

    End 19.9.2025

--- S1/main.cpp
+++ S1/main.cpp
@@ -15,6 +15,24 @@ void funzione(int i)
 //
 //}
 
+template<typename T>
+T getmax2(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+
+auto getmax(auto x, auto y)
+{
+    if (x > y) {
+        return x;
+    }
+    return y;
+}
+
+double getPi()
+{
+    return 3.14;
+}
 
 int main()
 {
@@ -45,4 +63,19 @@ int main()
 
     funzione(3);
     funzione(3.14);
+    int k = 3;
+    // Inizializzazione C++
+    int z(8);
+    // Inizializzazione C++ (2)
+    int p = {42};
+    // Inizializzazione C++ (3), da preferire
+    int f{5};
+
+    int r = 4.56; // Warning: narrowing conversion
+    int t(4.56);  // Warning: narrowing conversion
+    //int h{4.56};  // Error: narrowing conversion (il codice non compila)
+
+    //int pi{getPi()};
+    auto pi_v2{getPi()};
+    auto c = 2 * r * pi_v2;
 }
